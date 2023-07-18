912e1c24cc632d7840ef57e2e9e584474f1e6286
crowtorakawa@gmail.com~ <crowtorakawa@gmail.com~>
2023-07-18 13:55:53 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bodyparser = require('body-parser')
-
+const flash = require('connect-flash')
 const app = express()
 const routes = require('./routes')
 const usePassport = require('./config/passport')
@@ -26,10 +26,13 @@ app.use(bodyparser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
 usePassport(app)
+app.use(flash())
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg') // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg') // 設定 warning_msg 訊息
   next()
 })
 app.use(routes)
